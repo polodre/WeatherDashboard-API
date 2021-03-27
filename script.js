dc20cd9e0c3ae0b3b0645c0872eb5b6b55117476
Andre Lucas <andre.lucas284@gmail.com>
2021-03-26 22:33:14 -0400

    js

---
+++ script.js
@@ -0,0 +1,18 @@
+var cityList = [];
+var cityname;
+
+initCityList();
+initWeather();
+
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
